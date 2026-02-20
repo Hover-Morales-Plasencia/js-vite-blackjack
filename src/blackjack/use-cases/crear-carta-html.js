7e2503e17786bcb94de2868717dcce71f41abb2e
Hover Morales <hovermoralesp@gmail.com>
2026-02-20 11:36:08 -0500

    Fix: usando BASE_URL para rutas de imágenes

--- src/blackjack/use-cases/crear-carta-html.js
+++ src/blackjack/use-cases/crear-carta-html.js
@@ -8,9 +8,11 @@ export const crearCartaHTML= (carta) =>{
 
     if(!carta) throw new Error('La carta es un argumento obligatorio')
 
+    const base = import.meta.env.BASE_URL;    
+
     // <img class="carta" src="assets/cartas/2C.png">
     const imgCarta = document.createElement('img');
-    imgCarta.src = `/js-vite-blackjack/cartas/${ carta }.png`; //3H, JD
+    imgCarta.src = `${base}/cartas/${ carta }.png`; //3H, JD
     imgCarta.classList.add('carta');
     
 
